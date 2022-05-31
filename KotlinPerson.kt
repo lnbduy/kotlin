f116d8fbcadbaffcef8dfa7644a50bf61d2a0265
Duy Le <baduykt@yahoo.com>
2022-05-31 21:34:03 +0700

    Update KotlinPerson

--- KotlinPerson.kt
+++ KotlinPerson.kt
@@ -7,28 +7,22 @@ data class KotlinPerson(val id: Long, val title: String, val firstName: String, 
 
     companion object {
         fun getAge(dateOfBirth: Calendar?): Int {
-            if (dateOfBirth == null) {
-                return -1
-            }
+            if (dateOfBirth == null) return -1
             val today: Calendar =  GregorianCalendar();
             val years: Int = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR);
-            if (dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.YEAR)) {
-                return years -1;
-            } else {
-                return years;
-            }
+            return if (dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.YEAR)) years -1 else years
         }
     }
-
-    fun getAge(): Int {
-        return Companion.getAge(dateOfBirth)
-    }
+    val age: Int
+    get() = getAge(dateOfBirth)
 }
 
 fun main(args: Array<String>) {
-    val john: KotlinPerson = KotlinPerson(1L, "Mr", "John", "Blue", GregorianCalendar(1977, 9,3))
+    val john = KotlinPerson(1L, "Mr", "John", "Blue", GregorianCalendar(1977, 9,3))
     val jane = KotlinPerson(2L, "Mrs", "Jane", "Green", null);
-    println("${john}'s age is ${john.getAge()}")
-    println("${jane}'s age is ${jane.getAge()}")
+    println("${john}'s age is ${john.age}")
+    println("${jane}'s age is ${jane.age}")
     println("The age of someone born on 3rd May 1988 is ${KotlinPerson.getAge(GregorianCalendar(1985,5,3))}")
+    val olderPerson = if (john.age > jane.age) john else jane
+    println("The older person was ${olderPerson}")
 }
